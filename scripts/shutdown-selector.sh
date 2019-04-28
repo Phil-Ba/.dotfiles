e3f9cb3408b502787f70e73925e0c9511428c330
Bayer, Philipp <p.bayer@freemail.hu>
2019-04-28 21:44:44 +0200

    Fix select mode and size

--- scripts/shutdown-selector.sh
+++ scripts/shutdown-selector.sh
@@ -3,7 +3,7 @@
 hibernate="Hibernate"
 shutdown="Shutdown"
 suspend="Suspend"
-selected=$(echo -e "$hibernate|$shutdown|$suspend" | rofi -sep '|' -dmenu -p "Select action" -only-match)
+selected=$(echo -e "$hibernate|$shutdown|$suspend" | rofi -sep '|' -dmenu -p "Select action" -no-custom -no-fixed-num-lines)
 
 echo $selected
 locker=$HOME/.config/i3/lock
