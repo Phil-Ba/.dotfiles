64150c6a5ebd94f5655f9fd243eb01ede1851a40
philba <p.bayer@freemail.hu>
2019-05-05 18:46:05 +0200

    Updated screenconfig

--- .config/.screenlayout/work-3-monitors.sh
+++ .config/.screenlayout/work-3-monitors.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3840x120 --rotate normal --output DP1 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
+$randr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3840x120 --rotate normal --output DP1 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
 sleep 1
 #i3-msg move workspace number 1 to output DVI-I-3-2
 #i3-msg move workspace number 2 to output DVI-I-2-1
