92b222519d8386c4d36ce05669b1a1195f032b4e
philba <p.bayer@freemail.hu>
2019-05-09 15:04:58 +0200

    Fixed screenlayout commands again

--- .config/.screenlayout/work-3-monitors.sh
+++ .config/.screenlayout/work-3-monitors.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-$randr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 3840x120 --rotate normal --output DP1 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 3887x120 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal
+
 sleep 1
 #i3-msg move workspace number 1 to output DVI-I-3-2
 #i3-msg move workspace number 2 to output DVI-I-2-1
