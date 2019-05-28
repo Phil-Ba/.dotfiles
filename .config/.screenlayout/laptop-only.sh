926538cdb4a5504436f8a5b146e6594c17414742
phi.ba <p.bayer@freemail.hu>
2019-05-28 16:10:26 +0200

    fixed screen names

--- .config/.screenlayout/laptop-only.sh
+++ .config/.screenlayout/laptop-only.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --off
 
 #sleep 1
 sh ~/.config/polybar/launch.sh
--- .config/.screenlayout/work-3-monitors.sh
+++ .config/.screenlayout/work-3-monitors.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 3887x120 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 3887x120 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-3-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-2-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal
 
 sleep 1
 #i3-msg move workspace number 1 to output DVI-I-3-2
