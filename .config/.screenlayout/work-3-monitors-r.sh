d0182577775354e6f5215203c3848f188ff74a60
phi.ba <p.bayer@freemail.hu>
2019-06-18 08:59:11 +0200

    Added reverse setup

---
+++ .config/.screenlayout/work-3-monitors-r.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 1920x1080 --pos 3887x120 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DVI-I-2-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-3-2 --primary --mode 1920x1200 --pos 1920x0 --rotate normal
+
+sleep 1
+#i3-msg move workspace number 1 to output DVI-I-3-2
+#i3-msg move workspace number 2 to output DVI-I-2-1
+i3-msg workspace number 1,move workspace to output DVI-I-2-1
+i3-msg workspace number 2,move workspace to output DVI-I-3-2
+#sleep 1
+sh ~/.config/polybar/launch.sh
